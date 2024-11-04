44fa9971b8062ba8c2e395ca0b1aba8fdef8850f
Dias <diasakbergen2205@gmail.com>
2024-11-04 18:05:05 +0500

    Initial Commit

---
+++ Tic Tac Tic Tac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Tic Tac
+//
+//  Created by Диас Акберген on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
